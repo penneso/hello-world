7e9bae45bc898bf8625acf7c900eeee6e717d75d
penneso <37552882+penneso@users.noreply.github.com>
2018-10-21 18:52:24 +0100

    Make changes to improve after code review

--- fuzzy_percentiles.py
+++ fuzzy_percentiles.py
@@ -14,15 +14,9 @@ def find_tot_balance(bins, tot_no_companies):
 
 def fill_bins(scores, bin_count):
     # Populates the bins with scores
-    bins = []
     counts = Counter(scores)
-    entries = sorted(set(scores))
-
-    for entry in entries:
-        bin = []
-        for count in range(counts[entry]):
-            bin.append(entry)
-        bins.append(bin)
+    bins = [[k]*v for k, v in sorted(counts.items())]
+    
     #Right now we have as many bins as there were different scores
     #now we need to reduce them until we have the required number of bins
     reduce_bins(bins, bin_count)
@@ -35,8 +29,7 @@ def reduce_bins(bins, bin_count):
     while len(bins) > bin_count:
 
         location_to_combine = find_small_bin_combos(bins)
-        bins[location_to_combine] = bins[location_to_combine] + bins[location_to_combine + 1]
-        del bins[location_to_combine + 1]
+        bins[location_to_combine].extend(bins.pop(location_to_combine + 1))
 
     return bins
 
@@ -45,9 +38,10 @@ def find_small_bin_combos(bins):
     #with the smallest sum, these are the best as the next step to combine
     min_length = len(bins[0]) + len(bins[1])
     location = 0
-    for i in range(len(bins) - 1):
-        if len(bins[i]) + len(bins[i + 1]) < min_length:
-            min_length = len(bins[i]) + len(bins[i+1])
+            
+    for i, b in enumerate(bins):
+        if len(b) + len(bins[i + 1]) < min_length:
+            min_length = len(b) + len(bins[i + 1])
             location = i
     return location
 
@@ -56,17 +50,12 @@ def import_csv(filename):
     with open(filename, 'rb') as f:
         return [int(item) for sublist in csv.reader(f) for item in sublist]
 
-def main():
-    bin_count = 100#input("Number of bins:")#5
-    filename = 'input_scores.csv'#'input_scores.csv'#input("Name of csv with scores:")#test.csv
+if __name__ == '__main__':
+    bin_count = 100#input("Number of bins:")
+    filename = 'input_scores.csv'#input("Name of csv with scores:")
     scores = import_csv(filename)
     bins = fill_bins(scores, bin_count)
-
     print(bins)
-    print(len(bins),find_tot_balance(bins, len(scores))/100)
+    print(len(bins), find_tot_balance(bins, len(scores))/100)
     
     return bins
-        
-
-if __name__ == '__main__':
-    main()
